d73f35634685ad8b71b6caa7a0498ca009e8e1c3
HJ-Cai <lj_chj@126.com>
2018-05-22 14:50:30 +0800

    Revert "fix bug"
    This reverts commit 1a345f8de93e6c53350d051e38634c51f855ec25.

--- HKAttributedTextView/HKAttributeTextView.m
+++ HKAttributedTextView/HKAttributeTextView.m
@@ -234,7 +234,7 @@
     
 }
 
-- (BOOL)textView:(UITextView *)textView shouldInteractWithURL:(NSURL *)URL inRange:(NSRange)characterRange {
+- (BOOL)textView:(UITextView *)textView shouldInteractWithURL:(NSURL *)URL inRange:(NSRange)characterRange interaction:(UITextItemInteraction)interaction {
     
     NSArray *urlComp = [URL.absoluteString componentsSeparatedByString:@"://"];
     NSArray *keys;
